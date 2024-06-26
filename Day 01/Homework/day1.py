2dbdc62072a1fb78bb218274fdadd3063a7e4c7b
Tornike823 <gegechkoritornike4@gmail.com>
2024-06-27 02:31:43 +0400

    Update day1.py

--- Day 01/Homework/day1.py
+++ Day 01/Homework/day1.py
@@ -1,3 +1,7 @@
-name = ("tornike")
-surname = ("gegechkori")
-print(name + " " + surname)
+name = ("tornike ")
+surname = ("gegechkori ")
+Hobby = ("chemi hobbi valorantis tamashi da programireba ")
+Country = ("me saqartvelodan var ")
+
+
+print("chmei saxelia " + name + surname + Hobby + "da " + Country)
