0866806f363e1464dca11290faed1c8590190e70
Tornike823 <gegechkoritornike4@gmail.com>
2024-06-27 03:10:44 +0400

    commit

--- Day 01/Homework/day1.py
+++ Day 01/Homework/day1.py
@@ -1,3 +1,14 @@
-name = ("tornike")
-surname = ("gegechkori")
-print(name + " " + surname)
+name = ("tornike ")
+surname = ("gegechkori ")
+Hobby = ("chemi hobbi valorantis tamashi da programireba ")
+Country = ("me saqartvelodan var ")
+
+
+print("chmei saxelia " + name + surname + Hobby + "da " + Country)
+
+
+print(5 + 5)
+print(12 * 35)
+print(500 // 3)
+print(22 ** 2)
+print(240 - 60)
