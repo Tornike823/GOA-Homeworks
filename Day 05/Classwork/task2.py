b8be03121f7d8bcd5899ffe9a1233ec4d21db00a
Tornike823 <gegechkoritornike4@gmail.com>
2024-07-01 22:20:30 +0400

    commit

---
+++ Day 05/Classwork/task2.py
@@ -0,0 +1,4 @@
+name = input("enter your name:")
+
+print(name)
+messege = input("nice to meet you tornike")
---
+++ Day 05/Classwork/task3.py
@@ -0,0 +1,7 @@
+name = input("enter your name:")
+surname = input("enter your surname:")
+age = input("enter your age:")
+
+print(name, surname, age)
+
+print("me var tornike gegechkori da var 15 ")
