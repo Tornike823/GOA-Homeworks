f4b44959d4e15c788c8cc4c80cf25c57870f2e1b
Tornike823 <gegechkoritornike4@gmail.com>
2024-07-13 16:08:11 +0400

    commit

---
+++ Day 09/Homework/tak2.py
@@ -0,0 +1,10 @@
+"""დავალება1: ეცადეთ input-ბით, და მათემატიკური ოპერაციებით შექმნათ კალკულატორი"""
+
+num1 = int(input("enter first number: "))
+num2 = int(input("enter second number: "))
+
+print(num1 + num2)
+print(num1 - num2)
+print(num1 * num2)
+print(num1 / num2)
+
---
+++ Day 09/Homework/task1.py
@@ -0,0 +1,9 @@
+"""დავალება: მომხმარებელს შემოატანინეთ 2 რიცხვი და გაკეტეთ მათემატიკური ოპერაციები"""
+
+num1= int(input("enter num1:"))
+num2= int(input("enter num2:"))
+
+print(num1 + num2)
+print(num1 - num2)
+print(num1 * num2)
+print(num1 / num2)
