73b3854c60b8e6231f3fa8423a1f9bea7327bcfc
Tornike823 <gegechkoritornike4@gmail.com>
2024-07-16 16:52:25 +0400

    commit

---
+++ Day 11/Classwork/task1.py
@@ -0,0 +1,5 @@
+name1 = input("enter your name:")
+
+name2 = "tornike"
+
+print(name1 == name2)
---
+++ Day 11/Homework/task1.py
@@ -0,0 +1,7 @@
+"""დავალები: გაკეთეთ სარიგსტრაციო ფორმა, მომხმარებელს შემოატანინეთ ემაილი და პაროლი, 
+თუ ემაილი და პაროლი ორივე შემოიტანა სწორად, დაგვიპრინტოს True ხოლო სხვა შემთხვევაში False"""
+
+Gmail = input("enter your Gmail:")
+Password = input("enter password:")
+
+print(Gmail == "tornikegegechk@gmail.com" or Password == 123)
