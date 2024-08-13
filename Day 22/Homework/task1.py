9459b4977daf7ed6008b9cfe9975a0aa6f62dafb
Tornike823 <gegechkoritornike4@gmail.com>
2024-08-13 17:36:52 +0400

    commit

---
+++ Day 22/Homework/task1.py
@@ -0,0 +1,11 @@
+""" შექმენით სია და შეიტანეთ 10 სხვადასხვა მონაცემთა ტიპის მნიშვნელობა, შემდეგ 5 მონაცემს  შეუცვალეთ   მნიშვნელობა და შემდეგ დაპრინტეთ მთლიანი სია."""
+
+food = ["burger" , "pizza" , 10 , 5 , "Noodels" , "Khinkali" , 20 , 17 , 14 , "xawapuri"]
+
+food[2] = "hot dog"
+food[3] = "cheeseburger"
+food[6]= "shawama"
+food[7] = "mwvadi"
+food[8]= "Chicken wings"
+
+print(food)
