c21a18dc0fe30637d42d37c9c2683153c65d3b5a
Tornike823 <gegechkoritornike4@gmail.com>
2024-09-14 13:11:46 +0400

    commit

--- Day 37/Homework/task1.py
+++
@@ -1,3 +0,0 @@
-"""Task 1: String Formatting
-Create a string named template with the following content: "Hello, {name}. Welcome to {place}."
-Use the format() function to replace {name} with "Alice" and {place} with "Wonderland". Store the result in a variable named formatted_string and print it."""
