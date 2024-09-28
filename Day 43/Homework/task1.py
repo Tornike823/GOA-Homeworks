d6ff2e25cb2efbd8fc088f916336c51eba85b34a
Tornike823 <gegechkoritornike4@gmail.com>
2024-09-28 17:36:36 +0400

    commit

---
+++ Day 43/Homework/task1.py
@@ -0,0 +1,7 @@
+"""Create a string named template with the following content: "Hello, {name}. Welcome to {place}."
+Use the format() function to replace {name} with "Alice" and {place} with "Wonderland". Store the result in a variable named formatted_string and print it."""
+
+
+template = "Hello, {name}. Welcome to {place}."
+formatted_string = template.format(name="Alice", place="Wonderland")
+print(formatted_string)
---
+++ Day 43/Homework/task4.py
@@ -0,0 +1,8 @@
+"""Create a string named quote with the following content: "To be or not to be, that is the question."
+Use the replace() function to replace the word "be" with "code". Store the result in a variable named modified_quote and print it.
+"""
+
+
+quote = "To be or not to be, that is the question."
+modified_quote = quote.replace("be", "code")
+print(modified_quote)
