4dcce6d1cca921386c29444d860f1cb455603f43
Tornike823 <gegechkoritornike4@gmail.com>
2025-01-24 04:53:44 +0400

    commit

---
+++ Day 79/Classwork/task1.js
@@ -0,0 +1,9 @@
+
+
+let namee = window.prompt("Whats youre name?")
+let age = window.prompt("How old are you?")
+let car = window.prompt("Wich car is your Favourite?")
+
+console.log(namee)
+console.log(age)
+console.log(car)
---
+++ Day 80/Classwork/task1.js
@@ -0,0 +1,7 @@
+let namee = window.prompt("Whats youre name?")
+let age = window.prompt("How old are you?")
+let car = window.prompt("where are you from")
+
+console.log(namee)
+console.log(age)
+console.log(car)
