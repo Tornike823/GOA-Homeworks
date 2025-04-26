9710459342966f4c765182f0dbeae7c2fba2d164
Duda._. <gegechkoritornike4@gmail.com>
2025-04-27 01:29:24 +0400

    Delete Goa directory

--- Goa/Day 96/Homework/task1.js
+++
@@ -1,6 +0,0 @@
-
-const book = {Name:"3 chad" , writtenby:"janeza" , coming:"2025" , janri:"magari" , price:"1$" };
-
-console.log("this book is " + book.Name + "and this book was writteb by" + book.writtenby + 
-"this book will come" + book.coming + "janri is " + book.janri + "and price will be" + book.price)
-
--- Goa/Day 97/Homework/task1.js
+++
@@ -1,5 +0,0 @@
-
-
-
-const book = {title:"3 chad" , author:"janeza" , pages:"125" , publishyear:"2026" }
-console.log(book)
