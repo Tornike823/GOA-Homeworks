a244af49ce65358a7f61b911577ba9dccdc55978
Duda._. <gegechkoritornike4@gmail.com>
2025-04-27 01:29:58 +0400

    Add files via upload

---
+++ Day 97/Homework/task1.js
@@ -0,0 +1,5 @@
+
+
+
+const book = {title:"3 chad" , author:"janeza" , pages:"125" , publishyear:"2026" }
+console.log(book)
---
+++ Day 98/Homework/task1.js
@@ -0,0 +1,14 @@
+
+function parametres(username , email , password){
+    this.name = username;
+    this.em = email;
+    this.pass = password;
+}
+
+console.log(parametres("TOka" , "Tornike@gmail.com"  , "567"))
+
+
+
+const all = new parametres("Tornike" , "gegechkori@gmial.com" , "123") 
+
+console.log(all)
